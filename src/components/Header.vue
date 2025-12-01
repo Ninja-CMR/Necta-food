dc1ac4b647cd26b0818a0020c946947025fd5dc3
MEVA'A MEVA'A <mevaa_vme@lao-sarl.cm>
2025-12-01 13:29:46 +0100

    Header et Hero section du site web Necta food

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    <div class="flex flex-row items-center shadow-lg justify-around text-center">
+      <a href="#home"><img src="../assets/logo.jpg" class="w-[100px]"></a>
+      <nav class ="flex flex-row justify-around  gap-5 bg-white p-[20px] font-bold">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#location">Location</a>
+        <a href="#contact">Contact</a>
+      </nav>
+      <button class="bg-red-800 p-[15px] text-white rounded-4xl font-bold">Order Now</button>
+
+    </div>
+
+  </header>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/view/Hero.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section id="home" class="relative w-full h-screen bg-cover bg-center ">
+    <div class="absolute inset-0 bg-gray-900 opacity-50"></div>
+
+    <div class="relative z-10 flex flex-col justify-center items-center h-full text-center text-white px-4">
+      <h1 class="text-5xl md:text-7xl font-bold mb-4 drop-shadow-lg text-orange-600 ">Fresh & Delicious</h1>
+      <h1 class="text-5xl md:text-7xl font-bold mb-4 drop-shadow-lg text-red-800 ">Food Delivered</h1>
+      <p class="text-lg md:text-2xl mb-6 drop-shadow-md">Experience authentic flavors made with the <br> ingredients, delivered to your doorstep</p>
+      <div class="flex flex-row gap-5 ">
+        <a href="#" class="bg-red-800 hover:bg-red-500 px-6 py-3 rounded-3xl text-lg font-semibold">Order via Whatsapp</a>
+        <a href="#" class="bg-orange-600 hover:bg-orange-500 px-6 py-3 rounded-3xl text-lg font-semibold">View menu</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
