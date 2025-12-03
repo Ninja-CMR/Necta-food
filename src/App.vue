c75582bee7dc5182a6025ab1e89274c5d814e96e
Ninja_CMR <oliviermevaa0@gmail.com>
2025-12-03 11:54:59 +0100

    modifier le main.ts

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup >
 import Header from './components/Header.vue'
 import Hero from  "./view/Hero.vue"
 import featuresDishes from './components/features/featuresDishes.vue';
