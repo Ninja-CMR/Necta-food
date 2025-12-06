876d6e49b0a11b7871ba68605bc90d7394bfa4d1
Ninja_CMR <oliviermevaa0@gmail.com>
2025-12-06 16:33:47 +0100

    modification et suppression de primevue dans le projet

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import PrimeVue from 'primevue/config' ;
-import Aura from '@primeuix/themes/aura' ;
 
 
 const app = createApp(App)
 
-app.use(PrimeVue ,
-    {unstyled: true }
-    , {theme : {
-        preset : Aura ,
-
-    }})
 app.mount('#app')
